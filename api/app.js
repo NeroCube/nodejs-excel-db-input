642e73f676a558b88b7ce92ba9f303446098fb36
Nero <nerocube.tw@gmail.com>
2018-09-30 23:35:20 +0800

    add upload route

--- api/app.js
+++ api/app.js
@@ -1,1 +1,28 @@
-var 
+var express = require('expres');
+var logger = require('morgan');
+var bodyParser = require('body-passer');
+var moment = require('moment');
+var envConfigs = require('./configs/env');
+var uploads = require('./routes/uploads');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json({limit: '100mb'}));
+
+app.get('/', function(req, res, next) {
+    res.status(200).end();
+});
+
+//
+app.use('/', function (req, res, next) {
+    if(typeof req.user !== 'undefined' && typeof req.user.username !== 'undefined') {
+        console.log(moment().format('YYYY-MM-DD HH:mm:ss') + ' [' + req.user.uername + ']');
+    }
+    else {
+        console.log(moment().format('YYYY-MM-DD H:mm:ss') + ' [user unknow]');
+    }
+    next();
+});
+
+//add router
+app.use('/upload', upolads)
